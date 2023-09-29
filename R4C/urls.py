88971a0493961d40b69d6ddd8c346e50bc50cf02
Blendi <dronramone@mail.ru>
2023-09-29 13:19:35 +0400

    Task 2

--- R4C/urls.py
+++ R4C/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robot/', include('robots.urls', namespace='robots')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- robots/views.py
+++ robots/views.py
@@ -1,13 +1,15 @@
-import json
+from robots.excel.config import FILENAME
+from robots.excel.excel_work import ExcelDistributor
 from robots.models import Robot
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+import json
 
 
 @csrf_exempt
 def add_robo(request):
-
+    """Добавить/Читать роботов"""
     if request.method == 'POST':
         new_robot = Robot(**json.loads(request.body))
         new_robot.clean_fields(exclude=['serial'])
@@ -17,3 +19,13 @@ def add_robo(request):
     else:
         data = serializers.serialize('json', Robot.objects.all())
         return HttpResponse(data, content_type='application/json')
+
+
+@csrf_exempt
+def get_excel(request):
+    ExcelDistributor.read_robot()
+    with open(FILENAME, 'rb') as f:
+        file = f.read()
+    response = HttpResponse(file, content_type='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename="report.xls"'
+    return response
